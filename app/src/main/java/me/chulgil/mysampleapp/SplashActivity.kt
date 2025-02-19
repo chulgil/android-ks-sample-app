d70cab6e5ca65fb4aab5875c0bece25e131a99da
CGLee <chulgil.lee@riman.com>
2025-02-20 00:40:03 +0900

    Feat: Add IntroActivity

---
+++ app/src/main/java/me/chulgil/mysampleapp/SplashActivity.kt
@@ -0,0 +1,28 @@
+package me.chulgil.mysampleapp
+
+import android.annotation.SuppressLint
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.lifecycle.lifecycleScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+
+@SuppressLint("CustomSplashScreen")
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_splash)
+
+        lifecycleScope.launch {
+            delay(3000)
+            startActivity(Intent(this@SplashActivity, MainActivity::class.java))
+            finish()
+        }
+    }
+}
---
+++ app/src/main/java/me/chulgil/mysampleapp/auth/IntroActivity.kt
@@ -0,0 +1,17 @@
+package me.chulgil.mysampleapp.auth
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import me.chulgil.mysampleapp.R
+
+class IntroActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_intro)
+
+    }
+}
